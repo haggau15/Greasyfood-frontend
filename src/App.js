e3eb16cebba963ffd448059d96a7cd4e4bf5a730
haggau15 <haggau94@gmail.com>
2025-08-18 13:01:39 +0200

    Smol change 7

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           method: 'GET'
         }
       );
-      const data = await response.json();
+      const data = await response.body;
       setData(data);
     };
     fetchData();
